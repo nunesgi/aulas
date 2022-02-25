3a4024bff833705cca5b98c29e26839fdf15d7fb
nunesgi <99336715+nunesgi@users.noreply.github.com>
2022-02-25 10:49:53 -0300

    Create codigo.ino

---
+++ aula4/codigo.ino
@@ -0,0 +1,47 @@
+const int led1=2;
+const int led2=3;
+const int led3=4;
+const int led4=5;
+const int led5=6;
+
+const int bot1=8;
+const int bot2=9;
+const int bot3=10;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  
+  pinMode(bot1, INPUT);
+  pinMode(bot2, INPUT);
+  pinMode(bot3, INPUT);
+
+  
+}
+int var1;digitalRead(botão2)
+int var2;digitalRead(botão3)
+int var3;digitalRead(botão4)
+
+void loop()
+{
+  
+}
+  void acendeTudo()
+  {
+  digitalWrite(led1,  HIGH);
+   digitalWrite(led2, HIGH);
+   digitalWrite(led3, HIGH);
+   digitalWrite(led4, HIGH);
+   digitalWrite(led5, HIGH);
+   
+ digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+ 
+}
