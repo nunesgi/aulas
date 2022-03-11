28c69dbb79ada9f40aa09185c50ddd8261ee009d
nunesgi <99336715+nunesgi@users.noreply.github.com>
2022-03-11 10:12:08 -0300

    Create codigo.ino

---
+++ aulas/ex1/codigo.ino
@@ -0,0 +1,16 @@
+// C++ code
+//
+const int led1=8;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(500);
+}
